90f8f01020f5bde3544e40306e0873b6f22af021
Jorge Aguilera <xperro@gmail.com>
2025-07-19 17:00:24 -0400

    more configurations for automatic deploy

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
 
-echo "Clean containers"
+set -e  
+echo "Cleaning containers..."
 sudo docker-compose down -v --remove-orphans
 
-echo "Delete previous"
+echo "Deleting previous backend image (ignore error if not found)..."
 sudo docker rmi buildmas_backend || true
 
+echo "Making config.sh executable..."
 sudo chmod +x backend/config.sh
 
-echo "Starting"
+echo "Starting containers..."
 sudo docker-compose up --build --no-cache
